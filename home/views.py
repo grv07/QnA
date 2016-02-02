e17bb81a56b8b5cd2c96b7d8efde5d249d4c1211
anshulbisht06 <anshul.bisht06@gmail.com>
2016-02-02 16:53:30 +0530

    registration added

--- home/views.py
+++ home/views.py
@@ -12,20 +12,21 @@ def register_user(request):
 	serializer = UserSerializer(data = request.data)
 	if serializer.is_valid():
 		serializer.save()
-		status1 = 'None'
 		return Response({'username':request.data.get('username'),'email':request.data.get('email')}, status = status.HTTP_200_OK)
+		# login_user(request)
 	else:
+		print serializer.errors
 		return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
 @permission_classes((AllowAny,))
 def login_user(request):
-
 	user = User.objects.get(username = request.data.get('username'))
 	if user:
 		user.check_password(request.data.get('password'))
 		token = generate_token(user)
+		print token
 		return Response({'username':request.data.get('username'),'email':request.data.get('email'),'token':token}, status = status.HTTP_200_OK)
 	else:
 		return Response({'msg':'Please validate inputs'}, status = status.HTTP_400_BAD_REQUEST)
