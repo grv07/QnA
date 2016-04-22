ca008ff6eddb4b5cc67792060cffcc4a0f671efe
anshulbisht06 <anshul.bisht06@gmail.com>
2016-04-22 14:24:48 +0530

    time vs question graph and rank added

--- home/views.py
+++ home/views.py
@@ -131,6 +131,7 @@ def get_user_result(request, test_user_id, quiz_key, attempt_no):
 	incorrect_question_list = sitting.get_incorrect_questions()
 	_filter_by_category = filter_by_category(sitting)
 	data = get_user_result_helper(sitting, test_user_id, quiz_key, request.GET.get('order', None), _filter_by_category, get_order_by)
+	data['rank'] = test_user.rank
 	data['start_time_IST'] = parse_datetime(data['start_time_IST']).strftime('%s')
 	data['end_time_IST'] = parse_datetime(data['end_time_IST']).strftime('%s')
 	data['analysis'] = { 'filter_by_category':{}, 'section_wise_results' :{}, 'question_vs_time_result_ideal':{}, 'question_vs_time_result_real':{}}
@@ -142,7 +143,6 @@ def get_user_result(request, test_user_id, quiz_key, attempt_no):
 	data['analysis']['section_wise_results'] = filter_by_section(quiz, unanswerd_question_list, incorrect_question_list)
 	data['analysis']['question_vs_time_result_ideal'] = {60: [85, 116], 61: ["5", 19], 62: ["lully", 66], 63: ["7", 36], 20: [64, 62], 21: [69, 70], 48: ["6", 47], 47: [82, 119], 29: ["76", 64], 146: ["ok", 61], 147: ["ok", 33], 144: ["wood", 59], 99: ["9.0", 110], 98: ["0.0", 111], 100: ["0.0", 87], 101: ["0.0", 134], 95: ["9.0", 115], 97: ["0.0", 109], 96: ["9.0", 92], 59: ["d", 17], 22: [74, 114], 17: [52, 118], 16: [49, 44], 19: [60, 136], 18: [56, 40], 57: ["20", 43], 56: ["20", 20], 34: ["IT", 49], 31: ["it", 25]}
 	data['analysis']['question_vs_time_result_real'] = {60: [85, 136], 61: ["5", 29], 62: ["lully", 86], 63: ["7", 56], 20: [64, 102], 21: [69, 100], 48: ["6", 37], 47: [82, 129], 29: ["76", 54], 146: ["ok", 51], 147: ["ok", 43], 144: ["wood", 49], 99: ["9.0", 110], 98: ["0.0", 121], 100: ["0.0", 117], 101: ["0.0", 114], 95: ["9.0", 105], 97: ["0.0", 119], 96: ["9.0", 112], 59: ["d", 27], 22: [74, 134], 17: [52, 98], 16: [49, 24], 19: [60, 126], 18: [56, 30], 57: ["20", 33], 56: ["20", 40], 34: ["IT", 89], 31: ["it", 45]}
-	data['rank'] = TestUser.objects.get(id = test_user_id).rank
 	# html = t.render(Context({'data': data }))
 	if data['view_format'] == 'pdf':
 		return
