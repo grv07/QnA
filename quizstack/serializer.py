4656c7019bf46fcf3288a2b333e35c12fc26d5c7
anshulbisht06 <anshul.bisht06@gmail.com>
2016-06-25 03:59:20 +0530

    subcategory_name[section_name] problem solved

--- quizstack/serializer.py
+++ quizstack/serializer.py
@@ -2,7 +2,18 @@ from rest_framework import serializers
 from .models import QuizStack
 
 class QuizStackSerializer(serializers.ModelSerializer):
-	subcategory = serializers.StringRelatedField(many=False)
 	class Meta:
 		model = QuizStack
-		exclude = ('created_date', 'updated_date', 'subcategory',)
+		exclude = ('created_date', 'updated_date',)
+
+
+
+# from rest_framework import serializers
+# from .models import QuizStack
+# from quiz.serializer import SubCategorySerializer
+
+# class QuizStackSerializer(serializers.ModelSerializer):
+# 	subcategory = SubCategorySerializer(many=False, blank=True)
+# 	class Meta:
+# 		model = QuizStack
+# 		exclude = ('created_date', 'updated_date',)
